f1c520b54afc42e776279be4986b5e943b9935b9
Ashutoshdocs <aksinghdocument@gmail.com>
2026-06-28 12:39:48 +0530

    Update app.py

--- DOCKERPIPELINE/app.py
+++ DOCKERPIPELINE/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Azure DevOps Docker Pipeline Success! check"
+    return "Azure DevOps Docker Pipeline Success! check  hh"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
